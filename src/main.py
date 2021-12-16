1d008d6cc0ccc7c364151c02703ff85a6b2efb57
Renan Santana <renan_ota@hotmail.com>
2021-12-16 19:39:46 -0300

    implement call_rpc route

--- src/main.py
+++ src/main.py
@@ -4,11 +4,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def root():
+def index():
     # Mocked methods just for tests
+    service_name = "Service Test"
     methods = [
         {"name": "method01", "params": []},
         {"name": "method02", "params": ["param_a"]},
         {"name": "method03", "params": ["param_a", "param_b"]},
     ]
-    return render_template("index.html", methods=methods)
+    return render_template(
+        "index.html",
+        methods=methods,
+        service_name=service_name
+    )
+
+@app.route("/call_rpc")
+def call_rpc():
+    pass
