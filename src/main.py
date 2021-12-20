85314664587504b27c37ca8bac55fa6df5be6720
Renan Santana <renan_ota@hotmail.com>
2021-12-19 23:25:14 -0300

    finalize api

--- src/main.py
+++ src/main.py
@@ -1,20 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from nameko.standalone.rpc import ClusterRpcProxy
+import service_inspector
 
+
+NAMEKO_CONFIG = {"AMQP_URI": "pyamqp://guest:guest@broker"}
 
 app = Flask(__name__)
+nameko_service_data = service_inspector.get_service_data()
 
-NAMEKO_CONFIG = {"AMQP_URI": "pyamqp://guest:guest@localhost"}
 
 @app.route("/")
 def index():
-    # Mocked methods just for tests
-    service_name = "Service Test"
-    methods = [
-        {"name": "method01", "params": []},
-        {"name": "method02", "params": ["param_a"]},
-        {"name": "method03", "params": ["param_a", "param_b"]},
-    ]
+    service_name = nameko_service_data['service_name']
+    methods = nameko_service_data['methods']
     return render_template(
         "index.html",
         methods=methods,
@@ -23,11 +21,18 @@ def index():
 
 @app.route("/call_rpc/<service_name>", methods=["POST"])
 def call_rpc(service_name):
-    data = request.get_json()
-    method_name = data["method"]
-    method_params = data["params"]
+    data = request.form.copy()
+    method_name = data.pop("method")
+    method_params = {}
+    for i in data:
+        # Remove prefix from param name
+        prefix = f"{method_name}_"
+        param_name = i.replace(prefix, '')
+        method_params.update({param_name: data[i]})
+
     with ClusterRpcProxy(NAMEKO_CONFIG) as cluster_rpc:
         method = getattr(cluster_rpc[service_name], method_name)
         response = method(**method_params)
         print(response)
-    return "hello"
+
+    return redirect(url_for("index"))
