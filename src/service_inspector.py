0188dc0a786559c63bdfc90ec6b8cebf77faea0c
Renan Santana <renan_ota@hotmail.com>
2021-12-19 23:25:06 -0300

    fix inspector

--- src/service_inspector.py
+++ src/service_inspector.py
@@ -2,34 +2,50 @@ import inspect
 import service
 
 
-members = inspect.getmembers(service)
-service_class_name = None
+def get_service_data() -> dict:
+    members = inspect.getmembers(service)
+    service_class_name = None
 
-for member in members:
-    if "service" in member[0].lower():
-        service_class_name = member[0]
+    for member in members:
+        if "service" in member[0].lower():
+            service_class_name = member[0]
 
-if service_class_name is None:
-    print("Nameko class not identified")
-    exit(1)
+    if service_class_name is None:
+        print("Nameko class not identified")
+        exit(1)
 
-service_class = getattr(service, service_class_name)
-members_of_class = inspect.getmembers(service_class)
-methods_and_params = {}
+    service_class = getattr(service, service_class_name)
+    members_of_class = inspect.getmembers(service_class)
+    methods_and_params = []
 
-for member in members_of_class:
-    # Ignore builtin methods
-    if member[0].startswith("__"):
-        continue
-    # Filter only functions
-    if not inspect.isfunction(member[1]):
-        continue
-    method_name = member[0]
-    methods_and_params[method_name] = []
-    params = inspect.signature(member[1])
-    for param in params.parameters:
-        if param == "self":
+    for member in members_of_class:
+        # Ignore builtin methods
+        if member[0].startswith("__"):
             continue
-        methods_and_params[method_name].append(param)
 
-print(methods_and_params)
+        if member[0] == "name":
+            service_name = member[1]
+
+        # Filter only functions
+        if not inspect.isfunction(member[1]):
+            continue
+
+
+        method_name = member[0]
+        method_data = {
+            'name': method_name,
+            'params': []
+        }
+
+        # Get params of method
+        params = inspect.signature(member[1])
+        for param in params.parameters:
+            if param == "self":
+                continue
+            method_data['params'].append(param)
+        methods_and_params.append(method_data)
+
+    return {
+        'service_name': service_name,
+        'methods': methods_and_params,
+    }
